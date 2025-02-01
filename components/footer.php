9eb51c2ae1ee0c69867726e11bbe7ae7052b23ee
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-01-31 23:41:09 -0500

    s

--- components/footer.php
+++ components/footer.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
- <footer>
-        <nav>
-            <a href="https://github.com/Aboomar524">GitHub</a>
-            <a href="https://aboomar524.github.io/">GitHub.io</a>
-            <a href="https://aboomar524.github.io/web215/">WEB215.io</a>
-            <a href="https://aboomar524.github.io/web250/">WEB250.io</a>
-            <a href="https://www.freecodecamp.org/AhmadAlhomair">freeCodeCamp</a>
-            <a href="https://www.codecademy.com/profiles/ahmadAlHomair9297602551">Codecademy</a>
-            <a href="https://jsfiddle.net/abobana1/cmsq9x2j/11/">JSFiddle</a>
-            <a href="https://www.linkedin.com/in/ahmad-al-homair/">LinkedIn</a>
-        </nav>
-        <p>
-            Designed by <a href="card.html" class="contract-text" target="_blank">Ahmad Alhomair</a> - &copy;2025
-        </p>
-        <p><em>"Be with God and you will see God with you"</em></p>
-    </footer>
+<footer>
+    <nav>
+        <a href="https://github.com/Aboomar524">GitHub</a>
+        <a href="https://aboomar524.github.io/">GitHub.io</a>
+        <a href="https://aboomar524.github.io/web215/">WEB215.io</a>
+        <a href="https://aboomar524.github.io/web250/">WEB250.io</a>
+        <a href="https://www.freecodecamp.org/AhmadAlhomair">freeCodeCamp</a>
+        <a href="https://www.codecademy.com/profiles/ahmadAlHomair9297602551">Codecademy</a>
+        <a href="https://jsfiddle.net/u/abobana1/fiddles/">JSFiddle</a>
+        <a href="https://www.linkedin.com/in/ahmad-al-homair/">LinkedIn</a>
+    </nav>
+    <p>
+        Designed by <a href="card.html" class="contract-text" target="_blank">Ahmad Alhomair</a> - &copy;2025
+    </p>
+    <p><em>"Be with God and you will see God with you"</em></p>
+</footer>
