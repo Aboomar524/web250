e2277bf6cd65d156e51c334caee362c5faabc89b
Ahmad <111832798+Aboomar524@users.noreply.github.com>
2025-03-08 05:52:47 -0500

    aa

--- components/header.php
+++ components/header.php
@@ -3,14 +3,14 @@
 <header>
   <h1>Ahmad Alhomair's Adventurous Anaconda | WEB250</h1>
   <nav>
-      <ul class="navbar">
-          <li><a href="index.php">Home</a></li>
-          <li><a href="introduction.php">Introduction</a></li>
-          <li><a href="contract.php">Contract</a></li>
-          <li><a href="multipage/superduper_php/">Multipage PHP</a></li>
-          <li><a href="multipage/superduper_static/">Multipage HTML</a></li>
-          <li><a href="joy_of_php/">joy_of_php</a></li>
-          <li><a href="joy_of_php/samsusedcars.html">Sam's Used Cars</a></li>
-      </ul>
+    <ul class="navbar">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="introduction.php">Introduction</a></li>
+      <li><a href="contract.php">Contract</a></li>
+      <li><a href="multipage/superduper_php/" target="_blank">Multipage PHP</a></li>
+      <li><a href="multipage/superduper_static/">Multipage HTML</a></li>
+      <li><a href="joy_of_php/">joy_of_php</a></li>
+      <li><a href="joy_of_php/samsusedcars.html">Sam's Used Cars</a></li>
+    </ul>
   </nav>
 </header>
